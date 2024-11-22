fd4e03ef6c2106e40fd2a9c2162492a8f1538893
Pedro Henrique Moreira <pedrhenr@c2r3p4.42sp.org.br>
2024-11-22 16:54:24 -0300

    feat(p2-pdm): adiciona chave da api

--- src/services/catApiService.js
+++ src/services/catApiService.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const searchLimit = 5;
 
+const API_KEY = 'live_Esz9tWf9drxYhW1RE7qm9t6hiNUk4LtqGCSMG0AXG9YACGDIEL1zkq78XOLhfMQb';
+
 export default axios.create({
+  headers: {
+    'x-api-key': API_KEY,
+  },
   baseURL: `https://api.thecatapi.com/v1/images/search?limit=${searchLimit}`,
 });
